cf7c030b72c6b9e4ad02b5dd4cacaed53987e9ba
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2015-05-12 11:14:04 -0400

    Also forcibly remove dangling vagrant VM state directories

--- clean.sh
+++ clean.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+hash -r
+
+dir="$( cd "${0%/*}" && env pwd -P )"
+statedir="${BCPC_DIR:-$dir}/vbox"
 
 for i in bcpc-{bootstrap,vm{1..3}} ; do
-  VBoxManage controlvm $i poweroff
-  VBoxManage unregistervm $i --delete
+  VBoxManage controlvm $i poweroff 2>/dev/null
+  VBoxManage unregistervm $i --delete 2>/dev/null
+  if [ -d "${statedir}/${i}" ] ; then
+    echo "Forcibly removing ${i} VM state directory" >&2
+    rm -rf "${statedir}/${i}"
+  fi
 done
-
-#TODO: figure out where vbox VM state directory is and blow that away
