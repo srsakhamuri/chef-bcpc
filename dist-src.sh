ea1fd2f1c39ee50ec6e3e2ddc6e0db3db0636490
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2015-08-05 15:01:16 -0400

    Add utilities to reset Chef server and re-chef entire cluster

--- dist-src.sh
+++ dist-src.sh
@@ -1,1 +1,2 @@
-rsync -axSHvP -e 'ssh -ostricthostkeychecking=no -i vbox/insecure_private_key' --exclude vbox --exclude vmware --exclude vbox/insecure_private_key --exclude .chef . vagrant@10.0.100.3:chef-bcpc
+keyfile=/Users/bcpc/build/chef-bcpc/bootstrap/vagrant_scripts/.vagrant/machines/bootstrap/virtualbox/private_key
+rsync -axSHvP -e "ssh -ostricthostkeychecking=no -i ${keyfile}" --exclude vbox --exclude vmware --exclude vbox/insecure_private_key --exclude .chef . vagrant@10.0.100.3:chef-bcpc
