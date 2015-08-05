20f0c58af19b9cc2314d91bcae4f2b1a0e30ccd4
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2015-08-05 15:47:12 -0400

    Add dynamic calculation of bootstrap ssh-key path

--- dist-src.sh
+++ dist-src.sh
@@ -1,2 +1,11 @@
-keyfile=/Users/bcpc/build/chef-bcpc/bootstrap/vagrant_scripts/.vagrant/machines/bootstrap/virtualbox/private_key
+#/bin/bash
+
+hash -r
+set -e
+
+dir="$(git rev-parse --show-toplevel)" || exit 2
+keyfile="$(
+  cd "${dir}/bootstrap/vagrant_scripts"
+  vagrant ssh-config bootstrap | awk '/Host bootstrap/,/^$/{ if ($0 ~ /^ +IdentityFile/) print $2}'
+)"
 rsync -axSHvP -e "ssh -ostricthostkeychecking=no -i ${keyfile}" --exclude vbox --exclude vmware --exclude vbox/insecure_private_key --exclude .chef . vagrant@10.0.100.3:chef-bcpc
