12337894f15d6cbdd8e98a8b50d5de919bd4099c
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-02-06 10:58:14 -0500

    Add rally support for mitaka
    Uses upstream PPA to source packages and splits deployment setup into
    another recipe.

--- cookbooks/bcpc/recipes/rally.rb
+++ cookbooks/bcpc/recipes/rally.rb
@@ -19,116 +19,37 @@
 
 # This recipe simply installs rally on the given node (bootstrap by default). The rally-setup.rb will set rally
 # up to be able to be ran.
-
-# Note: Added package default-jre so that java is already installed during the bootstrapping for JMeter
-
-rally_user = node['bcpc']['rally']['user']
-
-%w{
-    wget
-    build-essential
-    libssl-dev
-    libffi-dev
-    python-dev
-    libpq-dev
-    libxml2-dev
-    libxslt1-dev
-    default-jre
-}.each do |pkg|
-    package pkg do
-        action :upgrade
-    end
+apt_repository 'mitaka-staging-rally' do
+  uri node['bcpc']['repos']['mitaka-staging']
+  distribution node['lsb']['codename']
+  components ['main']
+  key 'ubuntu-cloud-archive-mitaka-staging-trusty.key'
 end
 
-cookbook_file "/tmp/rally-pip.tar.gz" do
-    source "rally-pip.tar.gz"
-    cookbook 'bcpc-binary-files'
-    owner "root"
-    mode 00444
+package 'rally' do
+  action :upgrade
 end
 
-cookbook_file "/tmp/rally-bin.tar.gz" do
-    source "rally-bin.tar.gz"
-    cookbook 'bcpc-binary-files'
-    owner "root"
-    mode 00444
-end
-
-cookbook_file "/tmp/rally.tar.gz" do
-    source "rally.tar.gz"
-    cookbook 'bcpc-binary-files'
-    owner "root"
-    mode 00444
-end
-
-pip_version = '7.0.3'
-
-cookbook_file "/tmp/python-pip_#{pip_version}_all.deb" do
-    source "python-pip_#{pip_version}_all.deb"
-    cookbook 'bcpc-binary-files'
-    owner "root"
-    mode 00444
-end
-
-dpkg_package "python-pip" do
-    source "/tmp/python-pip_#{pip_version}_all.deb"
-    action :install
-end
-
-bash "rally-pip-bin" do
-    user "root"
-    code <<-EOH
-        tar xvf /tmp/rally-pip.tar.gz -C /usr/local/lib/python2.7/dist-packages .
-        tar xvf /tmp/rally-bin.tar.gz -C /usr/local/bin .
-        #easy_install -H None -f /usr/local/lib/python2.7/dist-packages rally
-    EOH
-    not_if "test -d /usr/local/lib/python2.7/dist-packages/rally-0.0.4-py2.7.egg"
-end
-
-# Make sure these directories are present
-# If this directory changes then make sure to change rally-setup.rb to reflect the change
-directory "/var/lib/rally/database" do
-  owner 'root'
-  group 'root'
-  mode '0777'
-  action :create
-  recursive true
-end
-
-directory "/opt/rally" do
-  owner "#{rally_user}"
-  group "#{rally_user}"
-  mode '0755'
-  action :create
-end
-
-directory "/etc/rally" do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-end
-
-# Rally config file does not have any variables at the moment but is a template anyway.
 template "/etc/rally/rally.conf" do
     source "rally.conf.erb"
-    owner "root"
-    group "root"
+    owner node['bcpc']['rally']['user']
+    group node['bcpc']['rally']['user']
     mode 0664
 end
 
-# Extract the rally source tree so that the tests are available
-# If the 'mv' command fails then no worries. The returns [0, 1, 2] will allows the recipe to continue
-bash "install-rally-source" do
-    code <<-EOH
-        tar xvf /tmp/rally.tar.gz -C /opt/rally
-        mv /opt/rally/install_rally.sh /opt/rally/install_rally.sh.do_not_run
-        chown -R #{rally_user}:#{rally_user} /opt/rally
-    EOH
-    not_if "test -f /opt/rally/install_rally.sh.do_not_run"
-    returns [0, 1, 2]
+# Remove old local rally installation remnants
+#  - just leave stuff in /opt/rally.. it's in /opt!
+bins = %w{ rally-manage rally }
+install_dir = '/usr/local/bin'
+lib_dir = '/usr/local/lib/python2.7/dist-packages/rally'
+bins.each do |bin|
+  path = File.join(install_dir, bin)
+  file path do
+    action :delete
+  end
 end
 
-# Wanted to also setup rally here but 'rally deployment create' also verifies the endpoints which have not been
-# setup at this stage.
-#include_recipe "bcpc::rally-setup"
+directory lib_dir do
+  recursive true
+  action :delete
+end
