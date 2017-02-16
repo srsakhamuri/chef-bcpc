41e606e603a4677e2bff608ede4fbf1e7b0bde02
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-02-16 16:54:15 -0500

    Make mitaka default openstack release
    https://docs.openstack.org/mitaka/

--- cookbooks/bcpc/attributes/default.rb
+++ cookbooks/bcpc/attributes/default.rb
@@ -7,7 +7,7 @@ default['bcpc']['country'] = "US"
 default['bcpc']['state'] = "NY"
 default['bcpc']['location'] = "New York"
 default['bcpc']['organization'] = "Bloomberg"
-default['bcpc']['openstack_release'] = "liberty"
+default['bcpc']['openstack_release'] = "mitaka"
 # Can be "updates" or "proposed"
 default['bcpc']['openstack_branch'] = "proposed"
 # Should be kvm (or qemu if testing in VMs that don't support VT-x)
