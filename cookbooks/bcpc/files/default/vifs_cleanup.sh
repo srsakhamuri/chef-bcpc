84e326cf988ba95eb5fdfc17701e563153232f7c
Edmund Rhudy <erhudy@users.noreply.github.com>
2017-02-28 16:39:24 -0500

    Add general DB table cleanup script to replace vifs_cleanup.sh

--- cookbooks/bcpc/files/default/vifs_cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# cleans up stale virtual interfaces from nova-network tables
-# (otherwise nova-network restarts can take a very long time)
-mysql -e "USE nova; CREATE TABLE IF NOT EXISTS virtual_interfaces_archive LIKE virtual_interfaces; START TRANSACTION; INSERT virtual_interfaces_archive SELECT * FROM virtual_interfaces WHERE deleted > 0 AND instance_uuid NOT IN (SELECT uuid FROM instances WHERE deleted = 0); DELETE FROM virtual_interfaces WHERE id IN (SELECT id FROM virtual_interfaces_archive); COMMIT;"
