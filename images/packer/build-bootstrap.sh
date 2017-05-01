ecf19a88cfb241043112adfec463716acc0fa54c
David A. Desrosiers <setuid@gmail.com>
2017-05-01 15:26:30 -0400

    Updated the bootstrap wrapper to include a remote fetch of the correct checksum and inject it into the .json

--- images/packer/build-bootstrap.sh
+++ images/packer/build-bootstrap.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-KEYFILE="bootstrap_chef.id_rsa"
-rm ${KEYFILE}*
-ssh-keygen -N "" -f $KEYFILE
-cp ${KEYFILE}.pub files
-cp ${KEYFILE} ../..
+keyfile="bootstrap_chef.id_rsa"
+rm "${keyfile}"*
+ssh-keygen -N "" -f "$keyfile"
+cp "${keyfile}".pub files
+cp "${keyfile}" ../..
+
+u_series=trusty
+u_url="http://archive.ubuntu.com/ubuntu/dists/$u_series-updates/main/installer-amd64/current/images"
+u_checksum=$(curl --progress -s -f -L $u_url/SHA256SUMS | awk -v f=./netboot/mini.iso '$2 == f { print $1 }')
+
+sed -i 's/"iso_checksum":.*/"iso_checksum": "'"$u_checksum"'",/' bcpc-bootstrap.json
+
+# Or, if you want to use jq instead of sed
+# jq ".builders[].iso_checksum |= \"$ubuntu_checksum\"" bcpc-bootstrap.json > bcpc-bootstrap-output.json
+
 packer build bcpc-bootstrap.json
+# packer build bcpc-bootstrap-output.json
