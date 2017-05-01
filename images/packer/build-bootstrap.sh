4c692b121460ceb21bb3c6845a0f8070b29fc217
David A. Desrosiers <setuid@gmail.com>
2017-05-01 15:37:06 -0400

    #BetterCommentsMatter

--- images/packer/build-bootstrap.sh
+++ images/packer/build-bootstrap.sh
@@ -9,6 +9,8 @@ u_series=trusty
 u_url="http://archive.ubuntu.com/ubuntu/dists/$u_series-updates/main/installer-amd64/current/images"
 u_checksum=$(curl --progress -s -f -L $u_url/SHA256SUMS | awk -v f=./netboot/mini.iso '$2 == f { print $1 }')
 
+# Inject the upstream checksum into the .json, so it always matches what we downloaded
+# This removes the need to update the .json by-hand to keep the checksum current
 sed -i 's/"iso_checksum":.*/"iso_checksum": "'"$u_checksum"'",/' bcpc-bootstrap.json
 
 # Or, if you want to use jq instead of sed
