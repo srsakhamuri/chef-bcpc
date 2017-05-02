e993ad4375c337ed54ff98d9f4fb93d77c423bd0
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-05-02 12:49:22 -0400

    Include xinetd dependency from headnodes for haproxy
    xinetd resources only declared in the xinetd recipe are utilized by the
    haproxy-head recipe. This allows the latter recipe to now run in isolation.

--- cookbooks/bcpc/recipes/haproxy-head.rb
+++ cookbooks/bcpc/recipes/haproxy-head.rb
@@ -18,6 +18,8 @@
 #
 
 include_recipe "bcpc::default"
+# There is cyclical dependency upon some of checks - see /etc/xinetd.d/*chk
+include_recipe "bcpc::xinetd"
 include_recipe "bcpc::haproxy-common"
 
 concat_fragment "haproxy-main-config" do
