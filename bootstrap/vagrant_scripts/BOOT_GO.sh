21646198aef5dccb32055e93eff985568be7cf1c
David A. Desrosiers <desrod@ubuntu-mitaka.gnu-designs.com>
2017-09-29 12:58:55 -0400

    More $REPO_ROOT quoting cleanup

--- bootstrap/vagrant_scripts/BOOT_GO.sh
+++ bootstrap/vagrant_scripts/BOOT_GO.sh
@@ -38,38 +38,38 @@ echo
 # Source common bootstrap functions. This is the only place that uses a
 # relative path; everything henceforth must use $REPO_ROOT.
 source ../shared/shared_functions.sh
-export REPO_ROOT=$REPO_ROOT
+export REPO_ROOT="$REPO_ROOT"
 
 load_configs
 
 # Perform preflight checks to validate environment sanity as much as possible.
 echo "Performing preflight environment validation..."
-source $REPO_ROOT/bootstrap/shared/shared_validate_env.sh
+source "$REPO_ROOT"/bootstrap/shared/shared_validate_env.sh
 
 # Test that Vagrant is really installed and of an appropriate version.
 echo "Checking VirtualBox and Vagrant..."
-source $REPO_ROOT/bootstrap/vagrant_scripts/vagrant_test.sh
+source "$REPO_ROOT"/bootstrap/vagrant_scripts/vagrant_test.sh
 
 # Do prerequisite work prior to starting build, downloading files and
 # creating local directories. Proxy configuration is handled there as well.
 echo "Downloading necessary files to local cache..."
-source $REPO_ROOT/bootstrap/shared/shared_prereqs.sh
+source "$REPO_ROOT"/bootstrap/shared/shared_prereqs.sh
 
 # Terminate existing BCPC VMs.
 echo "Shutting down and unregistering VMs from VirtualBox..."
-$REPO_ROOT/bootstrap/vagrant_scripts/vagrant_clean.sh
+"$REPO_ROOT"/bootstrap/vagrant_scripts/vagrant_clean.sh
 
 # Create VMs in Vagrant and start them.
 echo "Starting local Vagrant cluster..."
-$REPO_ROOT/bootstrap/vagrant_scripts/vagrant_create.sh
+"$REPO_ROOT"/bootstrap/vagrant_scripts/vagrant_create.sh
 
 # Install and configure Chef on all Vagrant hosts.
 echo "Installing and configuring Chef on all nodes..."
-$REPO_ROOT/bootstrap/shared/shared_configure_chef.sh $CLUSTER_TYPE
+"$REPO_ROOT"/bootstrap/shared/shared_configure_chef.sh $CLUSTER_TYPE
 
 # Dump out OpenStack information for users if a converged cluster
 if [[ $CLUSTER_TYPE == 'converged' ]]; then
-  $REPO_ROOT/bootstrap/vagrant_scripts/vagrant_print_useful_info.sh
+  "$REPO_ROOT"/bootstrap/vagrant_scripts/vagrant_print_useful_info.sh
 fi
 
 echo "Finished in $SECONDS seconds"
