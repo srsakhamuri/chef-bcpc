7db6b34d24dcad79f5a1362251451f6dc80268f4
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-10-23 11:51:30 -0400

    Uncommitted legacy changes.

--- utils/dist-src.sh
+++ utils/dist-src.sh
@@ -6,6 +6,6 @@ set -e
 dir="$(git rev-parse --show-toplevel)" || exit 2
 keyfile="$(
   cd "${dir}/bootstrap/vagrant_scripts"
-  vagrant ssh-config bootstrap | awk '/Host bootstrap/,/^$/{ if ($0 ~ /^ +IdentityFile/) print $2}'
+  vagrant ssh-config vm-bootstrap | awk '/Host vm-bootstrap/,/^$/{ if ($0 ~ /^ +IdentityFile/) print $2}'
 )"
 rsync -axSHvP -e "ssh -ostricthostkeychecking=no -i ${keyfile}" --exclude vbox --exclude vmware --exclude vbox/insecure_private_key --exclude .chef . vagrant@10.0.100.3:chef-bcpc
