6f53c25ad0c60cd0fa5778ac1e6e24db0582c0de
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-10-29 12:42:59 -0400

    Off-load provider credentials to library

--- cookbooks/bcpc/providers/host_aggregate.rb
+++ cookbooks/bcpc/providers/host_aggregate.rb
@@ -23,33 +23,6 @@ def whyrun_supported?
   true
 end
 
-def openstack_cli
-  _proto = node['bcpc']['protocol']['keystone']
-  _dnsdomain = node['bcpc']['cluster_domain']
-  _port = node['bcpc']['catalog']['identity']['ports']['admin']
-  _uri = node['bcpc']['catalog']['identity']['uris']['admin']
-  admin_project_name = node['bcpc']['keystone']['admin']['project_name']
-  admin_role_name = node['bcpc']['keystone']['admin_role']
-  admin_username = node['bcpc']['keystone']['admin']['username']
-  admin_project_domain = node['bcpc']['keystone']['admin']['project_domain']
-  admin_user_domain = node['bcpc']['keystone']['admin']['user_domain']
-  admin_pass = get_config('keystone-local-admin-password')
-  auth_url = "#{_proto}://openstack.#{_dnsdomain}:#{_port}/#{_uri}/"
-  region = node['bcpc']['region_name']
-
-  args =  ["openstack",
-           "--os-project-name", admin_project_name,
-           "--os-project-domain-name", admin_project_domain,
-           "--os-user-domain-name", admin_project_domain,
-           "--os-project-name", admin_project_name,
-           "--os-username", admin_username,
-           "--os-compute-api-version", "2",
-           "--os-auth-url", auth_url,
-           "--os-region-name", region,
-           "--os-password", admin_pass]
-  return args
-end
-
 # calculate the hostname
 # See bloomberg/chef-bcpc#1069
 def hostname
