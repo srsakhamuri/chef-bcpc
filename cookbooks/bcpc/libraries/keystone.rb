5a855dfad849ecaef0609aab95005ea96fd8740e
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-11-14 15:53:15 -0500

    Modify execute_in_keystone_admin_context to pass-through process status info.

--- cookbooks/bcpc/libraries/keystone.rb
+++ cookbooks/bcpc/libraries/keystone.rb
@@ -1,4 +1,6 @@
-def execute_in_keystone_admin_context(cmd, debug=false)
+require 'shellwords'
+
+def execute_in_keystone_admin_context(cmd, debug=false, &block)
   new_env = ENV.reject {|name,val| name.start_with? 'OS_'}.tap do |env|
     env['OS_TOKEN']="#{get_config('keystone-admin-token')}"
     env['OS_URL']="#{node['bcpc']['protocol']['keystone']}://openstack.#{node['bcpc']['cluster_domain']}:#{node['bcpc']['catalog']['identity']['ports']['admin']}/#{node['bcpc']['catalog']['identity']['uris']['admin']}/"
@@ -9,7 +11,15 @@ def execute_in_keystone_admin_context(cmd, debug=false)
   EoS
   script = "set -x;\n" + script if debug
   # Call script with new environment
-  env(new_env){ %x[ #{script} ] }
+  env(new_env) do
+    cmd = 'bash -c ' + Shellwords.escape(script)
+    o, e, s = Open3.capture3(cmd)
+    if block_given?
+      yield o, e, s
+    else
+      o
+    end
+  end
 end
 
 def keystone_db_version
