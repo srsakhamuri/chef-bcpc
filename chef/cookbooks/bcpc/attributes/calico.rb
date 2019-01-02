6ff02509995b6ed50f4d71dfbac42b0716136ada
David Comay <david.comay@gmail.com>
2019-01-01 21:22:02 -0500

    Update Calico version to 3.4 (#1457)

--- chef/cookbooks/bcpc/attributes/calico.rb
+++ chef/cookbooks/bcpc/attributes/calico.rb
@@ -2,7 +2,7 @@
 # calico/calicoctl
 ###############################################################################
 
-default['bcpc']['calico']['repo']['url'] = 'http://ppa.launchpad.net/project-calico/calico-3.2/ubuntu'
+default['bcpc']['calico']['repo']['url'] = 'http://ppa.launchpad.net/project-calico/calico-3.4/ubuntu'
 default['bcpc']['calico']['remote']['file'] = 'calicoctl'
 default['bcpc']['calico']['remote']['source'] = "#{default['bcpc']['file_server']['url']}/calicoctl"
-default['bcpc']['calico']['remote']['checksum'] = '3396ee93361726eede85e3f86b256c80bf8d9d95e6ec37b6573fb44e7bf64e2f'
+default['bcpc']['calico']['remote']['checksum'] = '7017efab112a75ff7123ca0db62a52940e68bd6f1f2fe41ef08fe12dd4c89ca5'
