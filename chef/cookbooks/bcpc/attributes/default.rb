bfbb5ffc3222c8785fd5d69451d9d8b5cf45caa0
David Comay <david.comay@gmail.com>
2019-01-02 18:36:39 -0500

    Update etcd version to 3.3.10 (#1459)

--- chef/cookbooks/bcpc/attributes/default.rb
+++ chef/cookbooks/bcpc/attributes/default.rb
@@ -190,10 +190,10 @@ default['bcpc']['memcached']['connections'] = 10240
 # etcd
 ###############################################################################
 
-etcd_file = 'etcd-v3.3.9-linux-amd64.tar.gz'
+etcd_file = 'etcd-v3.3.10-linux-amd64.tar.gz'
 default['bcpc']['etcd']['remote']['file'] = etcd_file
 default['bcpc']['etcd']['remote']['source'] = "#{default['bcpc']['file_server']['url']}/#{etcd_file}"
-default['bcpc']['etcd']['remote']['checksum'] = '7b95bdc6dfd1d805f650ea8f886fdae6e7322f886a8e9d1b0d14603767d053b1'
+default['bcpc']['etcd']['remote']['checksum'] = '1620a59150ec0a0124a65540e23891243feb2d9a628092fb1edcc23974724a45'
 
 ###############################################################################
 # virtualbox
