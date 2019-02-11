8d6c3aeee2a74ca8a15db5a0d8866fd2d5227de4
Justin J. Pacheco <justinjpacheco@gmail.com>
2019-02-11 11:08:39 -0500

    Ansible refactoring (#1477)
    find_asset
    
    * new filter to find file assets by name and return file asset object
    
    cloud_vars
    
    * new ansible variable which includes cloud and chef environment variables
    
    cloud images
    
    * updated to use find_asset filter
    * each image to be uploaded is now iterated over individually
    * new ansible filter "osadmin" used to authenticate with openstack
    * refactored image upload/import to be more efficient
    
    chef_cookbooks
    
    * updated to use find_asset filter
    * cookbook uploads implemented in dedicated task
    
    file_server_assets
    
    * updated to use find_asset filter
    * file upload implemented in dedicated task

--- ansible/playbooks/roles/common/filter_plugins/util.py
+++ ansible/playbooks/roles/common/filter_plugins/util.py
@@ -25,15 +25,15 @@ def transit_interfaces(a, *args, **kw):
 
 def find_interface(facts, macaddress):
 
-  interfaces = facts['interfaces']
+    interfaces = facts['interfaces']
 
-  for interface in interfaces:
-    if interface == 'lo':
-      continue
-    if facts.get(interface,{}).get('macaddress', None) == macaddress:
-      return facts[interface]
+    for interface in interfaces:
+        if interface == 'lo':
+            continue
+        if facts.get(interface,{}).get('macaddress', None) == macaddress:
+            return facts[interface]
 
-  raise ValueError("could not find interface with mac: " + macaddress)
+    raise ValueError("could not find interface with mac: " + macaddress)
 
 def update_chef_node_host_vars(a, *args, **kw):
 
@@ -48,12 +48,51 @@ def update_chef_node_host_vars(a, *args, **kw):
 
     return node_details
 
+def find_asset(a, *args, **kw):
+
+    asset_to_find = a
+    assets = args[0]
+
+    for asset in assets:
+        if asset['name'] == asset_to_find:
+            return asset
+
+    raise ValueError("could not find {}".format(asset_to_find))
+
+def osadmin(a, *args, **kw):
+
+    cloud_vars = a
+    chef = cloud_vars['chef']
+    cloud = cloud_vars['cloud']
+
+    databags = chef['databags']
+    config = [databag for databag in databags if databag['id'] == 'config'][0]
+
+    os_username = 'admin'
+    os_password = config['openstack'][os_username]['password']
+    os_region_name = cloud['region']
+    os_auth_url = "https://{}:35357/v3".format(cloud['fqdn'])
+
+    return {
+        'OS_PROJECT_DOMAIN_ID': 'default',
+        'OS_USER_DOMAIN_ID': 'default',
+        'OS_PROJECT_NAME': 'admin',
+        'OS_USERNAME': os_username,
+        'OS_PASSWORD': os_password,
+        'OS_AUTH_URL': os_auth_url,
+        'OS_REGION_NAME': os_region_name,
+        'OS_IDENTITY_API_VERSION': 3,
+        'OS_VOLUME_API_VERSION': 3
+    }
+
 class FilterModule(object):
 
   filter_map = {
     'primary_ip': primary_ip,
     'transit_interfaces': transit_interfaces,
     'update_chef_node_host_vars': update_chef_node_host_vars,
+    'find_asset': find_asset,
+    'osadmin': osadmin
   }
 
   def filters(self):
