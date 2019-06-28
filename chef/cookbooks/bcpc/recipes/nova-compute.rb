725b856b49002de0e65efbf6bc5d77723eb53c29
Justin J. Pacheco <justinjpacheco@gmail.com>
2019-06-28 13:43:16 -0400

    configure libvirt to use ssh (#1540)
    - configure nova-compute to use the qemu+ssh schema when executing
       live migrations
    - configure libvirt to no longer listen for incoming tcp connections

--- chef/cookbooks/bcpc/recipes/nova-compute.rb
+++ chef/cookbooks/bcpc/recipes/nova-compute.rb
@@ -48,11 +48,18 @@ directory '/var/lib/nova/.ssh' do
   group 'nova'
 end
 
-file '/var/lib/nova/.ssh/authorized_keys' do
-  content Base64.decode64(config['nova']['ssh']['crt']).to_s
-  mode '644'
-  owner 'nova'
-  group 'nova'
+begin
+  nova_authkeys = []
+  nova_authkeys.push(Base64.decode64(config['nova']['ssh']['crt']).to_s)
+  # add roots public key for live migrations via libvirts qemu+ssh
+  nova_authkeys.push(Base64.decode64(config['ssh']['public']).to_s)
+
+  file '/var/lib/nova/.ssh/authorized_keys' do
+    content nova_authkeys.join("\n")
+    mode '644'
+    owner 'nova'
+    group 'nova'
+  end
 end
 
 file '/var/lib/nova/.ssh/id_ed25519' do
@@ -72,11 +79,6 @@ end
 # configure libvirt
 template '/etc/libvirt/libvirtd.conf' do
   source 'libvirt/libvirtd.conf.erb'
-  notifies :restart, 'service[libvirtd]', :immediately
-end
-
-cookbook_file '/etc/default/libvirtd' do
-  source 'libvirt/default'
   notifies :restart, 'service[libvirtd]', :immediately
 end
 
