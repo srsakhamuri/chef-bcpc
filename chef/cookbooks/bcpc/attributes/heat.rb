0ff3829e2aa2a5213166cad9d80ba1be6b7a3caf
Justin J. Pacheco <justinjpacheco@gmail.com>
2019-06-28 14:15:36 -0400

    disable openstack heat by default (#1541)

--- chef/cookbooks/bcpc/attributes/heat.rb
+++ chef/cookbooks/bcpc/attributes/heat.rb
@@ -2,6 +2,8 @@
 # heat
 ###############################################################################
 
+default['bcpc']['heat']['enabled'] = false
+
 # database
 default['bcpc']['heat']['db']['dbname'] = 'heat'
 default['bcpc']['heat']['database']['max_overflow'] = 10
--- chef/cookbooks/bcpc/recipes/heat.rb
+++ chef/cookbooks/bcpc/recipes/heat.rb
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+return unless node['bcpc']['heat']['enabled']
+
 region = node['bcpc']['cloud']['region']
 config = data_bag_item(region, 'config')
 
