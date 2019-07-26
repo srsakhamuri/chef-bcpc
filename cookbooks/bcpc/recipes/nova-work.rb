6c28e16a68584455b022caa5af2d5aad1262e983
Justin J. Pacheco <justinjpacheco@gmail.com>
2019-07-26 13:33:31 -0400

    added updated nova-network model.py that includes wait method (#1557)

--- cookbooks/bcpc/recipes/nova-work.rb
+++ cookbooks/bcpc/recipes/nova-work.rb
@@ -267,6 +267,16 @@ unless node['bcpc']['enabled']['neutron']
     shasums_after_apply  'python-nova-objects-instance-mitaka-AFTER.SHASUMS'
     only_if "dpkg --compare-versions $(dpkg -s python-nova | egrep '^Version:' | awk '{ print $NF }') ge 2:13.1.1 && dpkg --compare-versions $(dpkg -s python-nova | egrep '^Version:' | awk '{ print $NF }') lt 2:14.0.0"
   end
+
+  # patches nova-network to add a wait method
+  bcpc_patch 'python-nova-network-model-mitaka' do
+    patch_file           'python-nova-network-model-mitaka.patch'
+    patch_root_dir       '/usr/lib/python2.7/dist-packages'
+    shasums_before_apply 'python-nova-network-model-mitaka-BEFORE.SHASUMS'
+    shasums_after_apply  'python-nova-network-model-mitaka-AFTER.SHASUMS'
+    only_if "dpkg --compare-versions $(dpkg -s python-nova | egrep '^Version:' | awk '{ print $NF }') ge 2:13.1.1 && dpkg --compare-versions $(dpkg -s python-nova | egrep '^Version:' | awk '{ print $NF }') lt 2:14.0.0"
+    notifies :restart, 'service[nova-network]', :immediately
+  end
 end
 
 include_recipe 'bcpc::calico-compute' if node['bcpc']['enabled']['neutron']
